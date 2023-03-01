3baa901d7723fb39c9594af0f7f5628c75ab0232
a505036987 <117793844+a505036987@users.noreply.github.com>
2023-03-01 16:15:26 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "https://github.com/a505036987/aws-terraform-module-rovpc/module"
+
+  wssdev-vpc         = "10.158.96.0/20"
+  aws_region         = "eu-central-1"
+  public_sn_count    = 2
+  private_sn_count   = 2
+  workspace_sn_count = 2
+}
